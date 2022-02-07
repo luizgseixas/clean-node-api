c0ab7d5c949df8a6cf6631dc2ef74930fca88530
Luixinho <ticolucas7@gmail.com>
2022-02-07 07:36:23 -0300

    test: ensure SignUpController calls EmailValidator with correct email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -95,4 +95,21 @@ describe('SignUpController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
+
+  test('Should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    // essa função de mock altera o valor de retorno de uma função, já que só necessitamos de um retorno false nesse teste
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    sut.handle(httpRequest)
+    // função de mock que verifica se a função mockada com spy esta sendo chamada com o parametro passado
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
 })
