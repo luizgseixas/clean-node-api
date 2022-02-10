f89ba3c4739d0488f285d836e5ce814356c437d4
Luixinho <ticolucas7@gmail.com>
2022-02-10 08:27:07 -0300

    refactor: change file name

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,4 +1,4 @@
-import { EmailValidatorAdapter } from './email-validator'
+import { EmailValidatorAdapter } from './email-validator-adapter'
 import validator from 'validator'
 
 // mocando o retorno pra sempre ser true
@@ -14,7 +14,7 @@ const makeSut = (): EmailValidatorAdapter => {
 
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     // mockando o valor de retorno da função para sempre retornar false
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
@@ -22,13 +22,13 @@ describe('EmailValidator Adapter', () => {
   })
 
   test('Should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
 
   test('Should call validator with correct email', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid('any_email@mail.com')
     // aqui estou esperando que o isEmailSpy seja chamado com o valor passado, no caso o 'any_email@mail.com'
