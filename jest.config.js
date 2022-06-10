687be8b4479f24cbd3292395b8ef29d59d33cc6c
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-06-10 08:57:23 -0300

    feat: ensure mongodb reconnect if client is down

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootoDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,17 +3,23 @@ import { Collection, MongoClient } from 'mongodb'
 export const MongoHelper = {
   // setando como null porque no js se você coloca : em um objeto ele entende que você esta atribuindo um valor, então inicializamos como null e forçamos o tipo dele
   client: null as MongoClient,
+  url: null as string,
 
   async connect (url: string): Promise<void> {
+    this.url = url
     // variavel setada pela lib @shelf/jest-mongodb
     this.client = await MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
   },
 
   async disconnect (): Promise<void> {
     await this.client.close()
+    this.client = null
   },
 
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client?.isConnected()) {
+      await this.connect(this.url)
+    }
     return this.client.db().collection(name)
   },
 
