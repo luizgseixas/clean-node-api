95cf9916dc307523b6c9975b7cc3076b428e65b2
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-07-13 11:42:05 -0300

    refactor: change routes to use filesystem instead of feast-glob

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,12 +1,14 @@
 import { Express, Router } from 'express'
-import fg from 'fast-glob'
+import { readdirSync } from 'fs'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**routes.ts').map(async file => {
-    // import dentro de uma função quando já foi importado algo no header do arquivo deve ser feito assim
-    // não tem como puxar algo especifico de dentro do arquivo tem que ser export default, por isso é colocado parenteses em volta ().default()
-    (await import(`../../../${file}`)).default(router)
+  // import dentro de uma função quando já foi importado algo no header do arquivo deve ser feito assim
+  // não tem como puxar algo especifico de dentro do arquivo tem que ser export default, por isso é colocado parenteses em volta ().
+  readdirSync(`${__dirname}/../routes`).map(async file => {
+    if (!file.includes('.test.')) {
+      (await import(`../routes/${file}`)).default(router)
+    }
   })
 }
