0e515fff68ccd7c8840b0c252b75718f7499f0cf
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-08-15 16:23:38 -0300

    test: ensure AccountMongoRepository will not find an account if account by token faills

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -117,5 +117,12 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toBe('any_email@mail.com')
       expect(account.password).toBe('any_password')
     })
+
+    test('Should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token')
+      // verifica se o account não é null, undefined ou false
+      expect(account).toBeFalsy()
+    })
   })
 })
