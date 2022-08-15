3170fffa07ad6905855a8c6a523cb2c10b547620
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-08-15 14:03:57 -0300

    feat: ensure JwtAdapter call verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -11,6 +11,7 @@ export class JwtAdapter implements Encrypter, Decrypter {
   };
 
   async decrypt (value: string): Promise<string> {
+    await jwt.verify(value, this.secret)
     return null
   }
 }
