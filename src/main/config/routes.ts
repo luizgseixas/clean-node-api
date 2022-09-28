cbfd7cca2799908f3597941055af687364e57043
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-09-28 15:16:59 -0300

    fix: make controllers return the error message intead of the error name

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -7,7 +7,7 @@ export default (app: Express): void => {
   // import dentro de uma função quando já foi importado algo no header do arquivo deve ser feito assim
   // não tem como puxar algo especifico de dentro do arquivo tem que ser export default, por isso é colocado parenteses em volta ().
   readdirSync(`${__dirname}/../routes`).map(async file => {
-    if (!file.includes('.test.')) {
+    if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
