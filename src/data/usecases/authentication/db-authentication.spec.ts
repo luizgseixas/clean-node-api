b2331f90c99a63409072cce56a374a454ff908b2
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-09-28 14:53:57 -0300

    refactor: change models to use type alias instead of interfaces

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -50,7 +50,7 @@ const makeUpdateAccessTokenRepository = (): UpdateAccessTokenRepository => {
   return new UpdateAccessTokenRepository()
 }
 
-interface SutTypes {
+type SutTypes = {
   sut: DbAuthentication
   loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
   hashComparerStub: HashComparer
